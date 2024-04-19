17cd65f3d971a6e87065f14bb34c411ed4bdfe7e
MrAlfonzo <s23mactalfr@venta.lv>
2024-04-19 11:53:56 +0300

    Add:added CRUD service interface

---
+++ src/main/java/lv/venta/service/IProductCRUDService.java
@@ -0,0 +1,20 @@
+package lv.venta.service;
+
+import java.util.ArrayList;
+
+import lv.venta.model.Product;
+
+public interface IProductCRUDService {
+	//CRUD - Create, Retrieve, Update, Delete;
+	
+	public abstract void create(Product product);
+	
+	public abstract Product retrieveById(int id);
+	
+	public abstract ArrayList<Product> retrieveAll();
+	
+	public abstract void update(int id, Product product);
+	
+	public abstract void deleteById(int id);
+
+}
