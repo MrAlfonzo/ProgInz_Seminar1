78ed065155c0d35f4e6cd1e2a9a1eefc2a3741d0
MrAlfonzo <s23mactalfr@venta.lv>
2024-05-10 10:25:34 +0300

    ADD: Updated CRUD and HTML for product insert and update

--- src/main/java/lv/venta/controller/ProductCRUDController.java
+++ src/main/java/lv/venta/controller/ProductCRUDController.java
@@ -1,16 +1,20 @@
-
 package lv.venta.controller;
+
+import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import lv.venta.model.Product;
-import lv.venta.service.IProductCRUDService;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.validation.Valid;
+import lv.venta.model.Product;
+import lv.venta.service.IProductCRUDService;
 
 @Controller
 @RequestMapping("/product/crud")
@@ -19,35 +23,89 @@ public class ProductCRUDController {
 	@Autowired
 	private IProductCRUDService crudService;
 
-	@GetMapping("/all") //localhost:8080/product/crud/all
-	public String getProductCRUDAll(Model model) 
-	{
+	@GetMapping("/all") // localhost:8080/product/crud/all
+	public String getProductCRUDAll(Model model) {
 		try {
-			model.addAttribute("mydata", crudService.retrieveAll());
-			return "product-show-all-page";//tiks parādīta producty-show-all-page.html ar visiem produktiem
+			ArrayList<Product> allProducts = crudService.retrieveAll();
+			model.addAttribute("mydata", allProducts);
+			return "product-show-all-page";// tiks parādīta producty-show-all-page.html ar visiem produktiem
 		} catch (Exception e) {
 			model.addAttribute("mydata", e.getMessage());
 			return "error-page";
 		}
 	}
-	
-	@GetMapping("/all/{id}") //localhost:8080/product/crud/one?id=2
-	public String getproductCRUDOne(@RequestParam("id") int id, Model model) {
+
+	@GetMapping("/one") // localhost:8080/product/crud/one?id=2
+	public String getProductCRUDOne(@RequestParam("id") int id, Model model) {
 		try {
 			Product foundProduct = crudService.retrieveById(id);
 			model.addAttribute("mydata", foundProduct);
-			return "product-show-one-page";
+			return "product-show-one-page";// tiks parādīta product-show-one-page.html lapa
 		} catch (Exception e) {
 			model.addAttribute("mydata", e.getMessage());
 			return "error-page";
 		}
 	}
-	
-	@GetMapping("/insert") //localhost:8080/product/crud/insert
-	public String getProductCrudInsert(Model model) {
-		model.addAttribute("product)", new Product());
-	}
-	
-	
 
+	@GetMapping("/all/{id}") // localhost:8080/product/crud/all/2
+	public String getProductCRUDById(@PathVariable("id") int id, Model model) {
+		try {
+			Product foundProduct = crudService.retrieveById(id);
+			model.addAttribute("mydata", foundProduct);
+			return "product-show-one-page";// tiks parādīta product-show-one-page.html lapa
+		} catch (Exception e) {
+			model.addAttribute("mydata", e.getMessage());
+			return "error-page";
+		}
+	}
+
+	@GetMapping("/insert") // localhost:8080/product/crud/insert
+	public String getProductCRUDInsert(Model model) {
+
+		model.addAttribute("product", new Product());
+		return "product-insert-page"; // tiks parādīta product-insert-page.html lapa ar iedotu tuksu produktu
+
+	}
+
+	@PostMapping("/insert")
+	public String postProductCRUDInsert(@Valid Product product, BindingResult result) {// ienāk aizpildītais produkts
+		// vai ir kādi validācijas pāŗkāpumi
+		if (result.hasErrors()) {
+			return "product-insert-page"; // turpinām palikt product-insert-page.html lapā
+		} else {
+			crudService.create(product);
+			return "redirect:/product/crud/all";
+		}
+	}
+
+	@GetMapping("/update/{id}") // localhost:8080/product/crud/update/1
+	public String getProductCRUDUpdateById(@PathVariable(name = "id") int id, Model model) {
+		try {
+			Product productForUpdating = crudService.retrieveById(id);
+			model.addAttribute("product", productForUpdating);
+			model.addAttribute("id", id);
+			return "product-update-page";
+		} catch (Exception e) {
+			model.addAttribute("mydata", e.getMessage());
+			return "error-page";
+		}
+	}
+
+	@PostMapping("/update/{id}")
+	public String postProductCRUDUpdateById(@PathVariable("id") int id, @Valid Product product, BindingResult result,
+			Model model) {
+		if (result.hasErrors()) {
+			return "product-update-page";
+		} else {
+
+			try {
+				crudService.update(id, product);
+				return "redirect:/product/crud/all/" + id; // pārlecu uz /product/crud/all/{id} galapunktu
+			} catch (Exception e) {
+				model.addAttribute("mydata", e.getMessage());
+				return "error-page";
+			}
+		}
+
+	}
 }
